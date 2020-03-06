75f9fe6800d649c50c3b26f0b5eb63a90d888a51
ziyuan <hanziyuan08@outlook.com>
2020-03-06 18:15:44 +0800

    First commit

--- cmd/gfw-converter/main.go
+++ cmd/gfw-converter/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"encoding/base64"
+	"github.com/go-resty/resty/v2"
+	"github.com/pkg/errors"
+	"log"
+)
 
+func main() {
+	ssContent, err := ssd2ss()
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	log.Println(ssContent)
+}
+
+func ssd2ss(url string) (string, error) {
+	client := resty.New()
+	resp, err := client.R().Get(url)
+	if err != nil {
+		return "", errors.Wrap(err, "get ShadowsocksD subscription failed")
+	}
+
+	var decodedSsdSubscription []byte
+	decodedSsdSubscription, err = base64.RawURLEncoding.DecodeString(string(resp.Body())[6:])
+	if err != nil {
+		return "", errors.Wrap(err, "decode ShadowsocksD subscription failed")
+	}
+	return string(decodedSsdSubscription), nil
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Bpazy/gfw-converter
+
+go 1.13
+
+require (
+	github.com/go-resty/resty/v2 v2.2.0
+	github.com/pkg/errors v0.9.1
+)
